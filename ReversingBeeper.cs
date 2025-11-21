e665575b1f96ebb78922b65ce4c5da07ee42b248
James Gohl <james.gohl@gmail.com>
2025-11-21 14:42:28 +0000

    First commit.

--- ReversingBeeper.cs
+++ ReversingBeeper.cs
@@ -90,8 +90,9 @@ namespace ReversingBeeper
             src.minDistance = 8f;        
             src.maxDistance = 100f;       
             src.dopplerLevel = 0.2f;        
-            src.reverbZoneMix = 0f;       
-            src.spatialize = false;       
+            src.reverbZoneMix = 0f;
+            src.volume = 0.7f;
+            src.spatialize = true;       
             src.playOnAwake = false;
             return src;
         }
